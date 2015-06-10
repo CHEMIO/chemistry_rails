60ceefb8a6a95141e91cce2f6e688af99a9c31e2
Radil Radenkov <radil@buzzstarter.com>
2015-06-10 18:22:34 +0300

    add cas number validator

---
+++ lib/cas_number_validator.rb
@@ -0,0 +1,12 @@
+class CasNumberValidator < ActiveModel::EachValidator
+  def validate_each(record,attribute,value)
+    cas, num0, num1, control = value.match(/(\d{2,7})\-(\d\d)-(\d)/).to_a
+    unless cas
+      record.errors[attribute] << (options[:message] || "format is invalid")
+      return
+    end
+    num = (num0+num1).reverse.scan(/\d/).each_with_index.map {|n, i| n.to_i*(i+1) }.sum
+
+    record.errors[attribute] << (options[:message] || "is invalid cas number") unless num % 10 == control.to_i
+  end
+end
